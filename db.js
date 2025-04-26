070d0c7463237fe56918bfe17bffaf19a8239f6b
swananddeo3 <swananddeo3@gmail.com>
2025-04-26 04:11:40 +0000

    Update db.js with MySQL credentials

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 
 
 // Create a connection pool
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'Final_Project', // Replace with your MySQL username
-    password: 'Password@0312', // Replace with your MySQL password
-    database: 'Laptopdb', // Replace with your database name
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const pool = mysql.createPool({ 
+    host: 'localhost', // Do not change this host if you are using local server
+    user: 'Final_Project', // Do not change the user
+    password: 'Password@0312', // Do not change the password
+    database: 'Laptopdb', // Do not change the database name
+    waitForConnections: true, 
+    connectionLimit: 10, 
+    queueLimit: 0 
 });
 
-module.exports = pool; 
+module.exports = pool;
